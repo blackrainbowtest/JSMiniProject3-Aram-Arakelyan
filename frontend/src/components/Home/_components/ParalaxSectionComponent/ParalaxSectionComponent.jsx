7eb28d963868ef232b47df426952c2b6b8a48bcc
blackrainbowtest <araqelyan.aram.96@gmail.com>
2024-05-04 18:01:41 +0400

    fix ESLine warning

--- frontend/src/components/Home/_components/ParalaxSectionComponent/ParalaxSectionComponent.jsx
+++ frontend/src/components/Home/_components/ParalaxSectionComponent/ParalaxSectionComponent.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useTranslation } from "react-i18next";
 
 import { setLoad } from "../../../../features/Main/MainSlice";
